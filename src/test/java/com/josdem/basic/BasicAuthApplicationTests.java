b2e79b44b4b2781bc5ab0b19117e8318e7e0b140
josdem <joseluis.delacruz@gmail.com>
2023-08-31 21:00:00 -0400

    Starting new spring security basic authentication service project :)

---
+++ src/test/java/com/josdem/basic/BasicAuthApplicationTests.java
@@ -0,0 +1,26 @@
+package com.josdem.basic;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@Slf4j
+@SpringBootTest
+@RequiredArgsConstructor
+class BasicAuthApplicationTests {
+
+  private final ApplicationContext applicationContext;
+
+  @Test
+  @DisplayName("it loads the application")
+  void shouldLoadContext(TestInfo testInfo) {
+    log.info("Running: {}", testInfo.getDisplayName());
+    assertNotNull(applicationContext, "should have an application context");
+  }
+}
